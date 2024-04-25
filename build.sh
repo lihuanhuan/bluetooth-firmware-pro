bf4574fc366907dd75077434f854467635e91b39
Adam <424778940z@users.noreply.github.com>
2024-04-25 20:02:38 +0800

    CI config update, toolchain config update (#26)
    Signed-off-by: Adam BZH <adam@onekey.so>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ rm -rf artifacts_signed
 rm -rf _build
 # build
 mkdir -p _build
-cmake  -G 'Ninja'  -S ./  -B ./_build
+cmake  -G 'Ninja'  -S ./  -B ./_build -DTOOL_CHAIN_PREFIX=$TOOL_CHAIN_PREFIX
 cmake --build ./_build --config Release -- -j$(nproc)
 utils/hash.py -t bluetooth -f artifacts/OnekeyProBTFW_APP.bin > artifacts/sha256.txt
 # remove build folder
